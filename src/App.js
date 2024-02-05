72898a676cef609383d94b6ea191b5bf8dd05d69
tom <nqwrt@naver.com>
2024-02-05 20:23:06 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import MainHeader from "./components/MainHeader";
 import MainFooter from "./components/MainFooter";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div id="App">
       <MainHeader></MainHeader>
+      <Outlet />{/*⭐️ URL에 따라 변경되는 부분 ⭐️ */}
       <MainFooter></MainFooter>
     </div>
   );
